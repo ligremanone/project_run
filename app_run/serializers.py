b4a558a7d15059eed892b474e1717a614c07f261
Khuzin Rustam <ligreman@mail.ru>
2025-07-19 00:02:39 +0300

    add athlete nested serializer for run serializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,12 +4,6 @@ from app_run.models import Run
 from django.contrib.auth.models import User
 
 
-class RunSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Run
-        fields = "__all__"
-
-
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
 
@@ -26,3 +20,25 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_type(self, obj):
         return "coach" if obj.is_staff else "athlete"
+
+
+class AthleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "last_name",
+            "first_name",
+        ]
+
+
+class RunSerializer(serializers.ModelSerializer):
+    athlete_data = AthleteSerializer(
+        read_only=True,
+        source="athlete",
+    )
+
+    class Meta:
+        model = Run
+        fields = "__all__"
--- project_run/urls.py
+++ project_run/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app_run.urls")),
-]
+] + debug_toolbar_urls()
