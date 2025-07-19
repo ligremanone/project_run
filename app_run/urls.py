e4285850b3bae3242e702634c35ad5e10bf1bc77
Khuzin Rustam <ligreman@mail.ru>
2025-07-19 15:45:59 +0300

    add start and stop run API view

--- app_run/urls.py
+++ app_run/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details, RunViewSet, UsersTypeViewSet
+from app_run.views import (
+    company_details,
+    RunViewSet,
+    UsersTypeViewSet,
+    RunAPIStartView,
+    RunAPIStopView,
+)
 
 router = DefaultRouter()
 router.register(
@@ -15,4 +21,6 @@ router.register(
 urlpatterns = [
     path("company_details/", company_details),
     path("", include(router.urls)),
+    path("runs/<int:run_id>/start/", RunAPIStartView.as_view(), name="start_run"),
+    path("runs/<int:run_id>/stop/", RunAPIStopView.as_view(), name="stop_run"),
 ]
--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from django.contrib.auth.models import User
 from app_run.models import Run
@@ -23,6 +26,41 @@ def company_details(request: Request) -> Response:
 class RunViewSet(ModelViewSet):
     queryset = Run.objects.all().prefetch_related("athlete")
     serializer_class = RunSerializer
+
+
+class RunAPIStartView(APIView):
+    def post(self, request: Request, run_id: int) -> Response:
+        run = get_object_or_404(Run, id=run_id)
+        if run.status == Run.INITIAL:
+            run.status = Run.IN_PROGRESS
+            run.save()
+            return Response(
+                {"message": "Run started"},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"message": "Run already started or finished"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class RunAPIStopView(APIView):
+    def post(self, request: Request, run_id: int) -> Response:
+        run = get_object_or_404(
+            Run,
+            id=run_id,
+        )
+        if run.status == Run.IN_PROGRESS:
+            run.status = Run.FINISHED
+            run.save()
+            return Response(
+                {"message": "Run stopped"},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"message": "Run not started or already finished"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class UsersTypeViewSet(ReadOnlyModelViewSet):
