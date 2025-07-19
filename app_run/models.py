448e327177b91e1e7db584896e31862e027c0860
Khuzin Rustam <ligreman@mail.ru>
2025-07-19 15:45:08 +0300

    add status field to Run model

--- app_run/models.py
+++ app_run/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Run(models.Model):
+    INITIAL = "init"
+    IN_PROGRESS = "in_progress"
+    FINISHED = "finished"
+    STATUS_CHOICES = [
+        (INITIAL, "Initial"),
+        (IN_PROGRESS, "In progress"),
+        (FINISHED, "Finished"),
+    ]
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default=INITIAL,
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
